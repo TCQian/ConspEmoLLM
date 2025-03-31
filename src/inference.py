c3cfcbac1741422579c6c1a768e6347a90198987
tanchiaqian <66521590+TCQian@users.noreply.github.com>
2025-03-31 09:57:37 +0800

    handle max length

--- src/inference.py
+++ src/inference.py
@@ -101,10 +101,17 @@ if __name__ == "__main__":
     print("Load model successfully")
 
     batch_size = args.batch_size
+    max_input_tokens = model.config.max_position_embeddings - max_new_tokens
     with open(args.predict_file, "w", encoding="utf-8") as write_f:
         for i in range(0, len(instruction_list), batch_size):
             batch_data = instruction_list[i : i + batch_size]
-            inputs = tokenizer(batch_data, return_tensors="pt", padding=True)
+            inputs = tokenizer(
+                batch_data,
+                return_tensors="pt",
+                padding=True,
+                truncation=True,
+                max_length=max_input_tokens,
+            )
             input_ids = inputs.input_ids.to(device)
             attention_mask = inputs.attention_mask.to(device)
 
