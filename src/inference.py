7efbe18e6c5ff4540f2b037fba7bb7a686ec5d7f
tanchiaqian <66521590+TCQian@users.noreply.github.com>
2025-03-31 09:16:20 +0800

    revise inference file to save memory

--- src/inference.py
+++ src/inference.py
@@ -1,7 +1,7 @@
 import torch
 from tqdm import tqdm
 from transformers import LlamaTokenizer, AutoTokenizer, AutoModelForCausalLM, AutoConfig
-from peft import  PeftModel
+from peft import PeftModel
 import argparse
 import pandas as pd
 import json
@@ -9,10 +9,11 @@ import os
 import random
 import numpy as np
 
+
 def seed_everything(seed=23):
-    print("seed",seed)
+    print("seed", seed)
     random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
@@ -20,20 +21,20 @@ def seed_everything(seed=23):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_name_or_path', type=str, required=True)
-parser.add_argument('--ckpt_path', type=str, default=None)
-parser.add_argument('--use_lora', action="store_true")
-parser.add_argument('--llama', action="store_true")
-parser.add_argument('--infer_file', type=str, required=True)
-parser.add_argument('--predict_file', type=str, required=True)
-parser.add_argument('--batch_size', type=int, required=True)
-parser.add_argument('--seed', type=int, required=True)
+parser.add_argument("--model_name_or_path", type=str, required=True)
+parser.add_argument("--ckpt_path", type=str, default=None)
+parser.add_argument("--use_lora", action="store_true")
+parser.add_argument("--llama", action="store_true")
+parser.add_argument("--infer_file", type=str, required=True)
+parser.add_argument("--predict_file", type=str, required=True)
+parser.add_argument("--batch_size", type=int, required=True)
+parser.add_argument("--seed", type=int, required=True)
 args = parser.parse_args()
 
 seed_everything(args.seed)
 
-if args.ckpt_path is None or args.ckpt_path == '':
-        args.ckpt_path = args.model_name_or_path
+if args.ckpt_path is None or args.ckpt_path == "":
+    args.ckpt_path = args.model_name_or_path
 
 max_new_tokens = 1024
 generation_config = dict(
@@ -43,23 +44,20 @@ generation_config = dict(
     do_sample=True,
     num_beams=1,
     repetition_penalty=1.2,
-    max_new_tokens=max_new_tokens
+    max_new_tokens=max_new_tokens,
 )
-
 
 infer_data = pd.read_json(args.infer_file, lines=True)
 instruction_list = infer_data.apply(
     lambda row: pd.Series(
-        {'instruction': f"Human: \n" + row['instruction'] + "\n\nAssistant:\n"}
-    ), axis=1
-)['instruction'].to_list()
+        {"instruction": f"Human: \n" + row["instruction"] + "\n\nAssistant:\n"}
+    ),
+    axis=1,
+)["instruction"].to_list()
 
-if __name__ == '__main__':
-    load_type = torch.float16 #Sometimes may need torch.float32
-    if torch.cuda.is_available():
-        device = torch.device(0)
-    else:
-        device = torch.device('cpu')
+if __name__ == "__main__":
+    load_type = torch.float16  # Sometimes may need torch.float32
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     if args.llama:
         tokenizer = LlamaTokenizer.from_pretrained(args.model_name_or_path)
@@ -73,39 +71,50 @@ if __name__ == '__main__':
     model_config = AutoConfig.from_pretrained(args.model_name_or_path)
 
     if args.use_lora:
-        base_model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, torch_dtype=load_type, device_map='auto')
-        model = PeftModel.from_pretrained(base_model, args.ckpt_path, torch_dtype=load_type)
+        base_model = AutoModelForCausalLM.from_pretrained(
+            args.model_name_or_path, torch_dtype=load_type, device_map="auto"
+        )
+        model = PeftModel.from_pretrained(
+            base_model, args.ckpt_path, torch_dtype=load_type
+        )
     else:
-        model = AutoModelForCausalLM.from_pretrained(args.ckpt_path, torch_dtype=load_type, config=model_config, device_map='auto')
+        model = AutoModelForCausalLM.from_pretrained(
+            args.ckpt_path,
+            torch_dtype=load_type,
+            config=model_config,
+            device_map="auto",
+        )
 
-    if device==torch.device('cpu'):
+    if device.type == "cpu":
         model.float()
 
     model.eval()
     print("Load model successfully")
+
     batch_size = args.batch_size
-    responses = []
-    with open(args.predict_file, 'w', encoding="utf-8") as write_f:
+    with open(args.predict_file, "w", encoding="utf-8") as write_f:
         for i in range(0, len(instruction_list), batch_size):
-            #if i <= 71424:
-            #    continue 
-            batch_data = instruction_list[i: min(i + batch_size, len(instruction_list))]
-            inputs = tokenizer(batch_data, return_tensors="pt",padding=True)
-            #print("inputs", inputs)
-            #print("length", len(inputs),len(inputs[0]))
+            batch_data = instruction_list[i : i + batch_size]
+            inputs = tokenizer(
+                batch_data, return_tensors="pt", padding=True, truncation=True
+            )
             input_ids = inputs.input_ids.to(device)
             attention_mask = inputs.attention_mask.to(device)
-            generation_output = model.generate(
-                    input_ids = input_ids,
-                    attention_mask = attention_mask,
-                    **generation_config
-                )   
+
+            with torch.no_grad():
+                generation_output = model.generate(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
+                    **generation_config,
+                )
+
             for j in range(generation_output.shape[0]):
-                response = tokenizer.decode(generation_output[j], skip_special_tokens=True, spaces_between_special_tokens=False)
-                data_one = {}
-                data_one["output"] = response
-                write_f.write(json.dumps(data_one, indent=None, ensure_ascii=False) + "\n")
-                responses.append(response)
-                print(response)
-                print("j",j)
-            print("i",i)
+                response = tokenizer.decode(
+                    generation_output[j],
+                    skip_special_tokens=True,
+                    spaces_between_special_tokens=False,
+                )
+                data_one = {"output": response}
+                write_f.write(
+                    json.dumps(data_one, indent=None, ensure_ascii=False) + "\n"
+                )
