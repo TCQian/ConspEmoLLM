cfd33b153ec2fc88874cfa2e1107358d4cef37c1
tanchiaqian <66521590+TCQian@users.noreply.github.com>
2025-03-31 23:41:53 +0800

    reduce text length further

--- loco/main.py
+++ loco/main.py
@@ -20,8 +20,8 @@ processed_lst = []
 for i, row in f1.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Determine if the text is a conspiracy theory. Classify it into one of the following two classes: 0. non-conspiracy. 1. conspiracy.\nText: {}\nClass:\n".format(
         loco_text
     )
@@ -34,8 +34,8 @@ for i, row in f1.iterrows():
 for i, row in f2.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Determine if the text is a conspiracy theory. Classify it into one of the following two classes: 0. non-conspiracy. 1. conspiracy.\nText: {}\nClass:\n".format(
         loco_text
     )
@@ -55,8 +55,8 @@ processed_lst = []
 for i, row in f1.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Assign a numerical value between 0 (least E) and 1 (most E) to represent the intensity of emotion E expressed in the text.\nEmotion: joy\nText: {}\nIntensity Score:\n".format(
         loco_text
     )
@@ -69,8 +69,8 @@ for i, row in f1.iterrows():
 for i, row in f2.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Assign a numerical value between 0 (least E) and 1 (most E) to represent the intensity of emotion E expressed in the text.\nEmotion: joy\nText: {}\nIntensity Score:\n".format(
         loco_text
     )
@@ -89,8 +89,8 @@ processed_lst = []
 for i, row in f1.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Assign a numerical value between 0 (least E) and 1 (most E) to represent the intensity of emotion E expressed in the text.\nEmotion: fear\nText: {}\nIntensity Score:\n".format(
         loco_text
     )
@@ -103,8 +103,8 @@ for i, row in f1.iterrows():
 for i, row in f2.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Assign a numerical value between 0 (least E) and 1 (most E) to represent the intensity of emotion E expressed in the text.\nEmotion: fear\nText: {}\nIntensity Score:\n".format(
         loco_text
     )
@@ -124,8 +124,8 @@ processed_lst = []
 for i, row in f1.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Assign a numerical value between 0 (least E) and 1 (most E) to represent the intensity of emotion E expressed in the text.\nEmotion: anger\nText: {}\nIntensity Score:\n".format(
         loco_text
     )
@@ -138,8 +138,8 @@ for i, row in f1.iterrows():
 for i, row in f2.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Assign a numerical value between 0 (least E) and 1 (most E) to represent the intensity of emotion E expressed in the text.\nEmotion: anger\nText: {}\nIntensity Score:\n".format(
         loco_text
     )
@@ -159,8 +159,8 @@ processed_lst = []
 for i, row in f1.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Assign a numerical value between 0 (least E) and 1 (most E) to represent the intensity of emotion E expressed in the text.\nEmotion: sadness\nText: {}\nIntensity Score:\n".format(
         loco_text
     )
@@ -173,8 +173,8 @@ for i, row in f1.iterrows():
 for i, row in f2.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Assign a numerical value between 0 (least E) and 1 (most E) to represent the intensity of emotion E expressed in the text.\nEmotion: sadness\nText: {}\nIntensity Score:\n".format(
         loco_text
     )
@@ -193,8 +193,8 @@ processed_lst = []
 for i, row in f1.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Evaluate the valence intensity of the writer's mental state based on the text, assigning it a real-valued score from 0 (most negative) to 1 (most positive).\nText: {}\nIntensity Score:\n".format(
         loco_text
     )
@@ -207,8 +207,8 @@ for i, row in f1.iterrows():
 for i, row in f2.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Evaluate the valence intensity of the writer's mental state based on the text, assigning it a real-valued score from 0 (most negative) to 1 (most positive).\nText: {}\nIntensity Score:\n".format(
         loco_text
     )
@@ -228,8 +228,8 @@ processed_lst = []
 for i, row in f1.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Categorize the text into an ordinal class that best characterizes the writer's mental state, considering various degrees of positive and negative sentiment intensity. 3: very positive mental state can be inferred. 2: moderately positive mental state can be inferred. 1: slightly positive mental state can be inferred. 0: neutral or mixed mental state can be inferred. -1: slightly negative mental state can be inferred. -2: moderately negative mental state can be inferred. -3: very negative mental state can be inferred.\nText: {}\nIntensity Class:\n".format(
         loco_text
     )
@@ -242,8 +242,8 @@ for i, row in f1.iterrows():
 for i, row in f2.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Categorize the text into an ordinal class that best characterizes the writer's mental state, considering various degrees of positive and negative sentiment intensity. 3: very positive mental state can be inferred. 2: moderately positive mental state can be inferred. 1: slightly positive mental state can be inferred. 0: neutral or mixed mental state can be inferred. -1: slightly negative mental state can be inferred. -2: moderately negative mental state can be inferred. -3: very negative mental state can be inferred.\nText: {}\nIntensity Class:\n".format(
         loco_text
     )
@@ -263,8 +263,8 @@ processed_lst = []
 for i, row in f1.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Categorize the text's emotional tone as either 'neutral or no emotion' or identify the presence of one or more of the given emotions (anger, anticipation, disgust, fear, joy, love, optimism, pessimism, sadness, surprise, trust).\nText: {}\nThis text contains emotions:\n".format(
         loco_text
     )
@@ -277,8 +277,8 @@ for i, row in f1.iterrows():
 for i, row in f2.iterrows():
     doc_id = row["doc id"]
     loco_text = loco_dataset[doc_id]["txt"]
-    if len(loco_text.split(" ")) > 1024:
-        loco_text = " ".join(loco_text.split(" ")[:1024])
+    if len(loco_text.split(" ")) > 400:
+        loco_text = " ".join(loco_text.split(" ")[:400])
     prompt = "Task: Categorize the text's emotional tone as either 'neutral or no emotion' or identify the presence of one or more of the given emotions (anger, anticipation, disgust, fear, joy, love, optimism, pessimism, sadness, surprise, trust).\nText: {}\nThis text contains emotions:\n".format(
         loco_text
     )
