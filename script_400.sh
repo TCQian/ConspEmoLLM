78ad5554fb859839883c785a29044664d08b5672
tanchiaqian <66521590+TCQian@users.noreply.github.com>
2025-04-04 06:00:37 +0800

    add 400 experiments

---
+++ script_400.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sbatch sbatch_conspllm_400.sh
+sbatch sbatch_conspemollm_400.sh
+sbatch sbatch_emollm_joy_400.sh
+sbatch sbatch_emollm_fear_400.sh
+sbatch sbatch_emollm_anger_400.sh
+sbatch sbatch_emollm_sadness_400.sh
+sbatch sbatch_emollm_class_400.sh
+sbatch sbatch_emollm_sentiment_400.sh
+sbatch sbatch_emollm_emotion_400.sh
